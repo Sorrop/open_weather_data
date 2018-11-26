72822d9d363bbc71d1f534006c40cb2a77b366c6
Chris Klaoudianis <chris.klaou@gmail.com>
2018-11-26 13:06:24 +0200

    Close persistence channel when all writes to db are completed

--- src/open_weather_data/state/persistence.clj
+++ src/open_weather_data/state/persistence.clj
@@ -1,15 +1,20 @@
 (ns open-weather-data.state.persistence
-  (:require [clojure.core.async :as async :refer [chan close! <! go-loop]]
+  (:require [clojure.core.async :as async :refer [chan
+                                                  close!
+                                                  <!
+                                                  go-loop]]
             [open-weather-data.state.database :refer [database]]
             [open-weather-data.db-ops :refer [db-operation]]
             [mount.core :refer [defstate]]))
 
 (defn listen-to-persistence-events []
   (let [pers-chan (chan)]
-    (async/go-loop []
+    (async/go-loop [counter 0]
       (when-let [[action city data cities-count] (<! pers-chan)]
         (db-operation action city data database)
-        (recur)))
+        (if-not (= (inc counter) cities-count)
+          (recur (inc counter))
+          (async/close! pers-chan))))
     pers-chan))
 
 (defstate persistence-listener
